90820222b8b7efc2ea311bc0710b4b6131a9bcbe
David McCullough <dmccullough488@gmail.com>
2025-07-28 02:37:31 -0500

    conditionally rendering color when 'like' or 'retweet' is clicked

--- index.js
+++ index.js
@@ -49,30 +49,41 @@ function getFeedHtml() {
   let feedHtml = ""
 
   tweetsData.forEach(function (tweet) {
+
+    let likeIconClass = ''
+    if (tweet.isLiked) {
+      likeIconClass = 'liked'
+    }
+
+    let retweetIconClass = ''
+    if (tweet.isRetweeted) {
+      retweetIconClass = 'retweeted'
+    }
+
     feedHtml += `  
       <div class="tweet">
-            <div class="tweet-inner">
-                <img src="${tweet.profilePic}" class="profile-pic">
-                <div>
-                    <p class="handle">${tweet.handle}</p>
-                    <p class="tweet-text">${tweet.tweetText}</p>
-                    <div class="tweet-details">
-                        <span class="tweet-detail">
-                          <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
-                          ${tweet.replies.length}
-                        </span>
-                        <span class="tweet-detail">
-                          <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
-                          ${tweet.likes}
-                        </span>
-                        <span class="tweet-detail">
-                          <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
-                          ${tweet.retweets}
-                        </span>
-                    </div>   
-                </div>            
-            </div>
-      </div>`
+          <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+              <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                  <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                    ${tweet.replies.length}
+                  </span>
+                  <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweet.uuid} "></i>
+                    ${tweet.likes}
+                  </span >
+                  <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
+                    ${tweet.retweets}
+                  </span>
+                 </div >   
+              </div >            
+          </div >
+      </div > `
   })
   return feedHtml
 }
