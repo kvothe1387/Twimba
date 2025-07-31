b9fe9de9565415de2632234891939436fcca46e9
David McCullough <dmccullough488@gmail.com>
2025-07-30 20:30:17 -0500

    created handleReplyClick func to used the uuid stored in 'replyId' to take ctrl of div and toggle 'hidden' class on click

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ document.addEventListener('click', function (e) {
   }
   else if (e.target.dataset.retweet) {
     handleRetweetClick(e.target.dataset.retweet)
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply)
   }
 })
 
@@ -42,6 +44,10 @@ function handleRetweetClick(tweetId) {
   }
   targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
   render()
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 
@@ -100,7 +106,7 @@ function getFeedHtml() {
                  </div>   
               </div>            
           </div>
-          <div id="replies-${tweet.uuid}">
+          <div class="hidden" id="replies-${tweet.uuid}">
             ${repliesHtml}
           </div>
       </div > `
